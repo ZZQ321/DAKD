2ad313f5a05edfbcd629f85d2e29e02fd04e80f4
ZZQ321 <zhongqiangz@163.com>
2023-03-07 20:59:15 +0800

    default

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 python ensemble.py --cfg configs/PACS_ens.yaml
 for gamma in 1.0
 do
-python distill.py --cfg configs/PACS_dis.yaml --gamma ${gamma} --tag feat_mul
+python distill.py --cfg configs/PACS_dis.yaml --gamma ${gamma}
 done
 
 python ensemble.py --cfg configs/PACS_ens.yaml
 for gamma in 1.0
 do
-python distill.py --cfg configs/PACS_dis.yaml --gamma ${gamma} --tag feat_mul
+python distill.py --cfg configs/PACS_dis.yaml --gamma ${gamma}
 done
